d7041144997ee29a0e9f9f4826220d0f59a590c8
Suresh Gopalakrishnan <g.suresh.ms@gmail.com>
2019-08-15 11:10:27 +0530

    Fibonacci

---
+++ src/org/test/If.java
@@ -0,0 +1,18 @@
+package org.test;
+
+public class If {
+	public static void main(String [] arg){
+		//defining variable
+		int a=10;int b=15;
+		if(a<=10){
+			if(b<15){
+		
+					System.out.println("VALID");
+			}
+			else{
+				System.out.println("INVALID");
+			}
+		}
+	}
+
+}
---
+++ src/org/test/OddNum.java
@@ -0,0 +1,21 @@
+package org.test;
+
+public class OddNum {
+	public  static void main (String[] arg){
+		int i =16; int j=23;
+		if(i%2==0){			
+			System.out.println("Even Number:"+"\t"+i);
+			}
+		else{
+			System.out.println("Invalid");
+		}
+			if (j%2!=0){
+				System.out.println("Odd Number :"+"\t"+j);
+			}
+		else {
+			System.out.println("INvalid");
+		}
+		
+	}
+
+}
